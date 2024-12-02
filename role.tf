bfe167ec2bc680c2df68a7f9fa6cadb616b05f29
Smruti Ranjan Moahnty <smruti.mohanty@infra360.io>
2024-12-02 18:35:00 +0530

    new-code-2/12/2024

--- role.tf
+++ role.tf
@@ -17,3 +17,36 @@ resource "aws_iam_role" "ecr_access" {
     ]
   })
 }
+
+resource "aws_iam_policy" "ecr_policy" {
+  for_each = toset(var.microservices)
+
+  name        = "ecr-policy-${each.key}"
+  description = "Policy to allow access to ECR for ${each.key} microservice"
+
+  # Define the permissions for interacting with ECR
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = [
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:PutImage",
+          "ecr:BatchGetImage",
+          "ecr:DescribeRepositories",
+          "ecr:CreateRepository"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecr_role_policy_attachment" {
+  for_each = toset(var.microservices)
+
+  policy_arn = aws_iam_policy.ecr_policy[each.key].arn
+  role       = aws_iam_role.ecr_access[each.key].name
+}
