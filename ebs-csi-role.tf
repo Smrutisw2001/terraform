f658f781a6ebfb305e26ac06e2266e2413aa7447
Smruti Ranjan Moahnty <smruti.mohanty@infra360.io>
2024-12-11 18:50:15 +0530

    mordify some codes

--- ebs-csi-role.tf
+++ ebs-csi-role.tf
@@ -1,17 +1,22 @@
+# Fetching the current AWS account ID
+data "aws_caller_identity" "current" {}
 
-# Data source to get the OIDC policy document
+data "aws_eks_cluster" "cluster" {
+  name = var.eks_cluster_name
+}
+
 data "aws_iam_policy_document" "ebs_csi_irsa" {
   statement {
     actions = ["sts:AssumeRoleWithWebIdentity"]
 
     principals {
       type        = "Federated"
-      identifiers = ["arn:aws:iam::218306567362:oidc-provider/oidc.eks.ap-south-1.amazonaws.com/id/97E4E32D9CFCF9D42953F512F5DBBAE8"]
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/${replace(data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer, "https://", "")}"]
     }
 
     condition {
       test     = "StringEquals"
-      variable = "oidc.eks.ap-south-1.amazonaws.com/id/97E4E32D9CFCF9D42953F512F5DBBAE8:sub"
+      variable = "${replace(data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer, "https://", "")}:sub"
       values = [
         "system:serviceaccount:kube-system:ebs-csi-controller-sa"
       ]
@@ -21,19 +26,17 @@ data "aws_iam_policy_document" "ebs_csi_irsa" {
   }
 }
 
-# Create the IAM Role
+
 resource "aws_iam_role" "ebs_csi" {
   name               = "ebs-csi-role-for-smruti-eks"
   assume_role_policy = data.aws_iam_policy_document.ebs_csi_irsa.json
 }
 
-# Attach the AmazonEBSCSIDriverPolicy to the IAM Role
 resource "aws_iam_role_policy_attachment" "AmazonEBSCSIDriverPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
   role       = aws_iam_role.ebs_csi.name
 }
 
-# Output IAM role ARN
 output "ebs_csi_role_arn" {
   value = aws_iam_role.ebs_csi.arn
 }
--- eks.tf
+++ eks.tf
@@ -13,7 +13,7 @@ module "eks" {
     kube-proxy             = {}
     vpc-cni                = {}
     aws-ebs-csi-driver  = {most_recent              = true
-      service_account_role_arn = "arn:aws:iam::218306567362:role/ebs-csi-role-for-smruti-eks"}
+      service_account_role_arn = aws_iam_role.ebs_csi.arn}
   }
 
   vpc_id                   = module.vpc.vpc_id
